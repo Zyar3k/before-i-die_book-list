ea724e3c6efb36ceae63ff7ffae4d28d67fdca3d
Arkadiusz Zygan <a.zygan@gmail.com>
2022-05-04 16:32:02 +0200

    Add route/controllers/errors pattern

--- server/server.js
+++ server/server.js
@@ -1,8 +1,23 @@
-require("dotenv").config();
-
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8888;
+const authRouter = require("./routes/auth");
+const booksRouter = require("./routes/books");
+
+const notFoundMiddleware = require("./middleware/not-found.js");
+const errorHandlerMiddleware = require("./middleware/error-handler.js");
+
+app.use(express.json());
+
+app.use("/admin/auth", authRouter);
+app.use("/admin/books", booksRouter);
+
+app.use("/", booksRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
