94947a4adbbce5e059d160d82b63f08d5fd9b8ca
Arkadiusz Zygan <a.zygan@gmail.com>
2022-05-05 12:30:05 +0200

    Finish server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv");
-dotenv.config();
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
 const app = express();
 const connectDB = require("./db/connect");
 const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6tpkm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 8888;
 const authRouter = require("./routes/auth");
 const booksRouter = require("./routes/books");
+dotenv.config();
 
 const notFoundMiddleware = require("./middleware/not-found.js");
 const errorHandlerMiddleware = require("./middleware/error-handler.js");
 
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 app.use("/admin/auth", authRouter);
 app.use("/admin/books", booksRouter);
