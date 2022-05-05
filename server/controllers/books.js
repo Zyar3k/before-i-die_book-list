f70c63c5c584e0864f3320533ea6fdd583157b06
Arkadiusz Zygan <a.zygan@gmail.com>
2022-05-05 12:22:31 +0200

    Add update book

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -1,6 +1,6 @@
 const Book = require("../models/Book");
 const { StatusCodes } = require("http-status-codes");
-const { NotFoundError } = require("../errors");
+const { NotFoundError, BadRequestError } = require("../errors");
 
 const getAllBooks = async (req, res) => {
   const books = await Book.find().sort("createdAt");
@@ -27,7 +27,44 @@ const createBook = async (req, res) => {
 };
 
 const updateBook = async (req, res) => {
-  res.send("update Book");
+  const {
+    body: {
+      title,
+      author,
+      page,
+      link,
+      list,
+      readed,
+      available,
+      desc,
+      adminRating,
+      rating,
+    },
+    params: { id: bookId },
+  } = req;
+
+  if (!title) {
+    throw new BadRequestError("Title is required");
+  }
+
+  const book = await Book.findOneAndUpdate(
+    { _id: bookId },
+    {
+      title,
+      author,
+      page,
+      link,
+      list,
+      readed,
+      available,
+      desc,
+      adminRating,
+      rating,
+    },
+    { new: true }
+  );
+
+  res.status(StatusCodes.OK).json({ book });
 };
 
 const deleteBook = async (req, res) => {
