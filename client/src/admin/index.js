fbc561a50804640c4fe3dfee497857e91995200f
Arkadiusz Zygan <a.zygan@gmail.com>
2022-05-06 17:26:56 +0200

    Edit server routes to create book

--- client/src/admin/index.js
+++ client/src/admin/index.js
@@ -1,5 +1,5 @@
 import BookActions from "./BookActions/BookActions";
-import CreateBook from "./CreateBook";
+import CreateBook from "./CreateBook/CreateBook";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
 import AdminStats from "./AdminStats";
