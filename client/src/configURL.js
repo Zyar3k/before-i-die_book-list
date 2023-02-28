136b27f1a3aacce0776785ed057e7834794b438c
Arek <52135197+Zyar3k@users.noreply.github.com>
2023-02-28 19:59:14 +0100

    Update configURL.js

--- client/src/configURL.js
+++ client/src/configURL.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "http://localhost:8888";
 // export const BASE_URL = "https://before-i-die-server.herokuapp.com";
-export const BASE_URL = "https://github.com/Zyar3k/before-i-die_book-list";
+export const BASE_URL = "https://server-bid.onrender.com";
